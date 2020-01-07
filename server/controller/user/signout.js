90d697cc183ac4999367b351cb9d976b297c6e7d
hyunsung <hslgood@gmail.com>
2020-01-08 02:19:23 +0900

    change

--- server/controller/user/signout.js
+++ server/controller/user/signout.js
@@ -2,6 +2,7 @@ module.exports = {
   post: (req, res) => {
     // TODO : 유저가 로그아웃했을 때, session 정보를 없애고, '/'로 redirect할 수 있도록 구현하세요.
     let sess = req.session;
+    console.log(sess);
     if (sess.userId) {
       req.session.destroy(err => {
         // console.log(req.session);
